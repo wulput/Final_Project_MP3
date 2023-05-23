e5d184eb967b80dd22f28af96ab4557fe2ad7289
wulput <wulandaripuput070@gmail.com>
2023-05-23 17:11:13 +0700

    commit tgl 23-05-2023

---
+++ app/src/main/java/com/danifitrianto/atrawica/OnBoard2Activity.kt
@@ -0,0 +1,11 @@
+package com.danifitrianto.atrawica
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OnBoard2Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_on_board2)
+    }
+}
---
+++ app/src/main/java/com/danifitrianto/atrawica/Payment1Activity.kt
@@ -0,0 +1,11 @@
+package com.danifitrianto.atrawica
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Payment1Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_payment1)
+    }
+}
